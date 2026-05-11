5c34ea03380a27befe0b4e668bc57a48191aafb8
9xbt <flippy632@gmail.com>
2026-05-11 17:42:22 +0200

    bentobox: oops

--- sysdeps/bentobox/generic/generic.cpp
+++ sysdeps/bentobox/generic/generic.cpp
@@ -49,7 +49,7 @@ namespace mlibc {
     }
 
     int Sysdeps<FutexWake>::operator()(int *pointer, bool all) {
-        return -__syscall2(SYS_futex_wake, (long)pointer, all ? INT_MAX : 1, 0);
+        return -__syscall2(SYS_futex_wake, (long)pointer, all ? INT_MAX : 1);
     }
 
     int Sysdeps<VmMap>::operator()(void *hint, size_t size, int prot, int flags,

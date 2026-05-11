491c8d93873836e6d4331a26f5a907e63c93b110
9xbt <flippy632@gmail.com>
2026-05-11 17:20:11 +0200

    bentobox: make __mlibc_thread_trampoline use new sysdeps

--- sysdeps/bentobox/generic/thread.cpp
+++ sysdeps/bentobox/generic/thread.cpp
@@ -5,15 +5,15 @@
 #include <mlibc/tcb.hpp>
 
 extern "C" void __mlibc_thread_trampoline(void *(*fn)(void *), Tcb *tcb, void *arg) {
-	mlibc::sys_tcb_set(tcb);
+	mlibc::sysdep<mlibc::TcbSet>(tcb);
 
 	while(__atomic_load_n(&tcb->tid, __ATOMIC_RELAXED) == 0)
-		mlibc::sys_futex_wait(&tcb->tid, 0, nullptr);
+		mlibc::sysdep<mlibc::FutexWait>(&tcb->tid, 0, nullptr);
 
 	tcb->invokeThreadFunc(reinterpret_cast<void *>(fn), arg);
 
 	__atomic_store_n(&tcb->didExit, 1, __ATOMIC_RELEASE);
-	mlibc::sys_futex_wake(&tcb->didExit);
+	mlibc::sysdep<mlibc::FutexWake>(&tcb->didExit, false);
 
-	mlibc::sys_thread_exit();
+	mlibc::sysdep<mlibc::ThreadExit>();
 }
